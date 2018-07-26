a2e7e5663545a285dcfe8b4a3d4f8bf444f6fe77
Jamil Nagua <jamil@MacBook-Pro-de-Jamil.local>
2018-07-25 23:50:56 -0500

    Integracion de Django con MySQL (creacion de la base de datos)

--- ProyectoSDCache/ProyectoSDCache/settings.py
+++ ProyectoSDCache/ProyectoSDCache/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProyectoSDCache.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ColeccionGIF',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -117,4 +121,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/‘
